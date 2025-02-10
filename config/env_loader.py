d35064485fdb4b9f16d6bb9e1b404c50f88f1592
W0JW0J <95754666+W0JW0J@users.noreply.github.com>
2025-02-10 20:43:30 +0100

    add: API key check

--- config/env_loader.py
+++ config/env_loader.py
@@ -9,6 +9,11 @@ def load_env():
     tracing_enabled = os.getenv('LANGCHAIN_TRACING_V2') # for LangSmith
     endpoint = os.getenv('LANGCHAIN_ENDPOINT')
     openai_api_key = os.getenv('OPENAI_API_KEY')
+    
+    # Check if OpenAI API key is valid
+    if not openai_api_key or openai_api_key == "YOUR_OPENAI_API_KEY":
+        print("Please set a valid OpenAI API key in the .env file.")
+        exit()
 
     #print(f'Tracing enabled: {tracing_enabled}')
     #print(f'LangSmith endpoint: {endpoint}')
